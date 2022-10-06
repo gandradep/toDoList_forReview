144d1ab61480baf882cf60c4d1e355c14f310164
gandradep <eventsonido@gmail.com>
2022-10-06 11:07:09 +0100

    Fixing linters

--- src/modules/index.test.js
+++ src/modules/index.test.js
@@ -1,13 +1,13 @@
-import {TaskList} from './taskList';
+import { TaskList } from './taskList.js';
 
 jest.mock('./taskList');
 describe('add/remove test', () => {
-const mylist = new TaskList;
-test('testing', () => {
-  expect(mylist.addTask('work',false,1)).toBe('work');
-});
-test('testing', () => {
-  const array = ['work', 'string', 'test'];
-  expect(mylist.removeTask(array, 1)[1]).toBe('test');
-});
+  const mylist = new TaskList();
+  test('testing', () => {
+    expect(mylist.addTask('work', false, 1)).toBe('work');
+  });
+  test('testing', () => {
+    const array = ['work', 'string', 'test'];
+    expect(mylist.removeTask(array, 1)[1]).toBe('test');
+  });
 });
