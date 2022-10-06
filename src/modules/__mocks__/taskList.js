4f6ff5e5bc4e160b43b9d82dc825e3b1391f10c5
gandradep <eventsonido@gmail.com>
2022-10-06 11:04:31 +0100

    Fixed linters

--- src/modules/__mocks__/taskList.js
+++ src/modules/__mocks__/taskList.js
@@ -1,21 +1,19 @@
+/* eslint-disable import/prefer-default-export */
 import { TaskTemplate } from './taskTemplate.js';
+
 export class TaskList {
   constructor() {
     this.taskArray = [];
   }
+
   addTask = (task, completed, index) => {
     const taskAdded = new TaskTemplate(task, completed, index);
     this.taskArray.push(taskAdded);
-    // localStorage.setItem('Task_list', JSON.stringify(this.taskArray));
-
-    return this.taskArray[this.taskArray.length-1].description;
+    return this.taskArray[this.taskArray.length - 1].description;
   }
+
   removeTask = (arr, index) => {
     arr.splice(index, 1);
-    // localStorage.setItem('Task_list', JSON.stringify(this.taskArray));
-    // element.classList.toggle('dNone');
-    // element.previousSibling.classList.toggle('dNone');
-    // this.displayList();
     return arr;
   }
 }
--- src/modules/index.test.js
+++ src/modules/index.test.js
@@ -1,15 +1,13 @@
+import {TaskList} from './taskList';
 
 jest.mock('./taskList');
-import {TaskList} from './taskList';
-import { TaskTemplate } from './taskTemplate.js';
-
 describe('add/remove test', () => {
-let mylist = new TaskList;
+const mylist = new TaskList;
 test('testing', () => {
   expect(mylist.addTask('work',false,1)).toBe('work');
 });
 test('testing', () => {
-  let array = ['work', 'string', 'test'];
+  const array = ['work', 'string', 'test'];
   expect(mylist.removeTask(array, 1)[1]).toBe('test');
 });
 });
